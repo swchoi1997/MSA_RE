21900adf4e5eee15a29119cbb04310cf2ff18139
sangwon <swchoi1997@naver.com>
2022-11-25 16:58:48 +0900

    feat: Eureka Server Register

---
+++ ecommerce/src/main/java/com/example/ecommerce/EurekaConfig.java
@@ -0,0 +1,9 @@
+package com.example.ecommerce;
+
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableEurekaServer
+public class EurekaConfig {
+}
